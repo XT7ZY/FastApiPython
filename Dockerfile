782a0e4b1c7e9c01c92e0895fc4dbe90cf9a570b
root <root@Debian.myguest.virtualbox.org>
2023-05-13 02:13:49 +0500

    Lab8

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.9.2
+
+LABEL maintainer="xtzzy@bk.ru"
+
+WORKDIR /code
+
+COPY ./requirements.txt /code/requirements.txt
+COPY ./src /code/src
+COPY ./alembic.ini /code/alembic.ini
+COPY ./migrations /code/migrations
+
+RUN pip install --no-cache-dir --upgrade -r requirements.txt
+
--- migrations/env.py
+++ migrations/env.py
@@ -3,6 +3,7 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
+from os import environ
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -14,6 +15,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+config.set_main_option('sqlalchemy.url', environ.get('DATABASE_URL'))
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
